3899720613cce96345b3886db854c5b8595dc4e8
Derrick Fyfield <derrickfyfield@DerricksMacMini.local>
2024-02-27 08:17:17 -0400

    Add a way to ignore private properties

---
+++ src/composables/useJsonStringify.ts
@@ -0,0 +1,9 @@
+// Helper to stringify json, but ignore private properties
+
+export function useJsonStringify(data:any){
+
+    const ignorePrivateProperties = (key:string, value:string) => {
+        return (key.startsWith("_")) ? undefined : value
+    }
+    return JSON.stringify(data, ignorePrivateProperties)
+}
